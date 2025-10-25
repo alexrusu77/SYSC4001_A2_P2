6c9a086e1fdec403c5be392fde8e1d228edb8638
alexrusu77 <alexrusu783@gmail.com>
2025-10-25 12:59:18 -0400

    Create process2.c

---
+++ process2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+  int counter = 0;
+  int cycles = 0;
+ 
+  while (1)
+    {
+      if (counter % 3 == 0)
+      {
+        printf("Process 2 (PID %d) - Cycle number: %d - %d is a multiple of 3\n", getpid(), cycles, counter);
+      }
+      else
+      {
+        printf("Process 2 (PID %d) - Cycle number: %d\n", getpid(), cycles);
+        counter--;
+        cycles++;
+        sleep(1);
+      }
+    }
+  
+}
